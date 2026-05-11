b1873487be6137000914b3a392c809d2fdaa535f
Narumi Iguchi <iguchi_n@tokium.jp>
2026-05-11 10:17:42 +0900

    fix: マイグレーションファイルの重複定義と行末空白を修正

--- db/migrate/20260511005729_add_null_constraint_to_todos_title.rb
+++ db/migrate/20260511005729_add_null_constraint_to_todos_title.rb
@@ -1,9 +1,5 @@
 class AddNullConstraintToTodosTitle < ActiveRecord::Migration[8.1]
   def change
+    change_column_null :todos, :title, false
   end
 end
-class AddNullConstraintToTodosTitle < ActiveRecord::Migration[8.1]                                                                                                                                              
-  def change                                                                                                                                                                                                    
-    change_column_null :todos, :title, false
-  end                                                                                                                                                                                                           
-end    
