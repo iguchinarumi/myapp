b6c334e3390ff53bbe7d2ccbad81b8e8e2cff484
Narumi Iguchi <iguchi_n@tokium.jp>
2026-05-12 15:17:16 +0900

    M3: CRUD実装 - Todo追加・一覧・更新・削除

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -16,6 +16,29 @@ class TodosController < ApplicationController
     end
   end
 
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      redirect_to @todo
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
+
   private
 
   def todo_params
