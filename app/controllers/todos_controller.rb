ffa550c49a9904bc8925ff07e7dcfdc094aee49d
Narumi Iguchi <iguchi_n@tokium.jp>
2026-05-15 15:19:32 +0900

    M4: ユーザー認証 - ログイン・ログアウト・アクセス制御

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,47 +1,49 @@
 class TodosController < ApplicationController
-  def index
-    @todos = Todo.all
-  end
+    before_action :require_login
 
-  def new
-    @todo = Todo.new
-  end
+    def index
+      @todos = current_user.todos
+    end
 
-  def create
-    @todo = Todo.new(todo_params)
-    if @todo.save
+    def new
+      @todo = current_user.todos.build
+    end
+
+    def create
+      @todo = current_user.todos.build(todo_params)
+      if @todo.save
+        redirect_to todos_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def show
+      @todo = current_user.todos.find(params[:id])
+    end
+
+    def edit
+      @todo = current_user.todos.find(params[:id])
+    end
+
+    def update
+      @todo = current_user.todos.find(params[:id])
+      if @todo.update(todo_params)
+        redirect_to @todo
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @todo = current_user.todos.find(params[:id])
+      @todo.destroy
       redirect_to todos_path
-    else
-      render :new, status: :unprocessable_entity
     end
-  end
 
-  def show
-    @todo = Todo.find(params[:id])
-  end
+    private
 
-  def edit
-    @todo = Todo.find(params[:id])
-  end
-
-  def update
-    @todo = Todo.find(params[:id])
-    if @todo.update(todo_params)
-      redirect_to @todo
-    else
-      render :edit, status: :unprocessable_entity
+    def todo_params
+      params.require(:todo).permit(:title, :completed)
     end
-  end
-
-  def destroy
-    @todo = Todo.find(params[:id])
-    @todo.destroy
-    redirect_to todos_path
-  end
-
-  private
-
-  def todo_params
-    params.require(:todo).permit(:title, :completed)
-  end
 end
